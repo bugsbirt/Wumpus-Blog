2f6a6ce3aaed1b522745efc594994017c1d3612d
Bugs <deezbird2768@gmail.com>
2024-08-26 20:38:23 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wumpus Blog",
-  description: "A blog all about Wumpus & their adventures!\n\n-# Not affiliated with Discord Inc.\n-# Discord is a registered trademark of Discord Inc.",
+  title: "Wumpus Blog ",
+  description: "A blog all about Wumpus & their adventures!",
 };
 
 export default function RootLayout({
@@ -20,8 +20,10 @@ export default function RootLayout({
       <head>
       <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
         <meta property="og:title" content="Wumpus Blog" />
+        <meta property="og:description" content="A **unofficial** blog all about Wumpus & their adventures!" />
         <meta property="og:image" content="/favicon.ico" />
         <meta property="og:url" content="https://wumpus.blog" />
+        <meta name="theme-color" content="#5b65e8" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://wumpus.blog/favicon.ico" />
         <meta property="og:image:type" content="image/png" />
