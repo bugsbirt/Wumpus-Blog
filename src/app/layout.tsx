610e482ac37b1d2f38812ce068c85ab2299cc383
Bugs <deezbird2768@gmail.com>
2024-08-26 20:14:12 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+      // discord meta data
+      <meta>
+        <meta property="og:title" content="Wumpus Blog" />
+        <meta property="og:description" content="A very cool blog about Wumpus" />
+        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:url" content="https://wumpus.blog" />
+        <meta property="og:type" content="website" />
+        <meta property="og:color" content="#5865F2" />
+        </meta>
       </head>      
       <body className={inter.className}>{children}</body>
     </html>
