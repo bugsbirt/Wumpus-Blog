4aaf47714b7765264a9a6d24e57099ca71e24017
Bugs <deezbird2768@gmail.com>
2024-08-27 12:16:44 +0100

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,7 @@ export function Navbarr() {
         <nav className="glassmorphism fixed w-full z-20 top-0 start-0 border-b border-zinc-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://utfs.io/f/786bdb1f-2aa6-4566-8396-0487e72b05a3-t2zciv.gif" className="h-8" alt="Flowbite Logo" />
+            <img src="https://utfs.io/f/786bdb1f-2aa6-4566-8396-0487e72b05a3-t2zciv.gif" className="h-8" alt="wumpy dumpies eating thingy bignsofisodfiosdf" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Wumpus Blog</span>
         </a>
         <a href="https://discord.gg/cTsCYmdg6z" className="glassmorphism inline-flex items-center space-x-2 px-4 py-2 bg-black border border-zinc-900 rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-zinc-950 focus:ring-2 focus:ring-zinc-950 focus:ring-opacity-50">
